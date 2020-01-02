00478ee3528047996a630609cf7fb397b01adce8
Werisu <wellysson35@gmail.com>
2020-01-02 13:54:43 -0300

    Carteõs dos contatos

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:contatos_de_pessoas/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main(){
 
   /* Armazenamento em SQLITE, armazenamento em tabela */
   runApp(MaterialApp(
-
+    home: HomePage(),
   ));
 
 }
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,114 @@
+import 'dart:io';
+
+import 'package:contatos_de_pessoas/helpars/contatos_helpars.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  ContactHelper helper = ContactHelper();
+
+  List<Contact> contacts = List();
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    Contact c = Contact();
+    c.id = 1;
+    c.name = "Wellysson Rocha";
+    c.email = "wellysson.rocha@estudante.ifto.edu.br";
+    c.phone = "62992510372";
+    c.image = null;
+    helper.updateContact(c);
+
+    helper.getAllContacts().then((list){
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index){
+          return _contactCard(context, index);
+        },
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index){
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].image != null ?
+                    FileImage(File(contacts[index].image)) :
+                        AssetImage("images/person.png")
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                        contacts[index].name ?? "",
+                        style: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.bold
+                        )
+                    ),
+                    Text(
+                        contacts[index].email ?? "",
+                        style: TextStyle(
+                            fontSize: 12.0,
+                        )
+                    ),
+                    Text(
+                        contacts[index].phone ?? "",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        )
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
